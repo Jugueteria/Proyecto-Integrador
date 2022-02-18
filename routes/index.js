68e5cf1a669240edc1f82e590d4d3b1cc3d3cd0e
Juanito1220 <proyectosoftwarejp@gmail.com>
2022-02-17 20:16:31 -0500

    Arreglo de vistas

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,5 @@ const mainController = require('../Controllers/mainControllers');
 
 /* GET home page. */
 router.get('/', mainController.index);
-router.get('/login', mainController.login);
-router.get('/productCart', mainController.productCart);
-router.get('/productDetail', mainController.productDetail);
-router.get('/register', mainController.register);
-router.get('/form_admin', mainController.form_admin);
-
-
 
 module.exports = router;
